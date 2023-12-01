0ccdfc4b2f702f6a31533b4d8ebf264fef3bf2e8
jankady <kaduch05@gmail.com>
2023-12-01 11:31:29 +0100

    creating style.css and connecting them to index.php, trying to solve grid for 2 colloms

--- components/header.php
+++ components/header.php
@@ -1,11 +1,13 @@
 <header>
     <div class="container-fluid text-center">
-        <div class="row">
+        <div class="row align-items-center h-70">
             <div class="col">
-                <a href="../img/products/MacBook.jpg"></a>
+                <img src="./img/products/MacBook.jpg" alt="selling MacBook ">
             </div>
             <div class="col">
-                2 of 2
+                <p>Exclusive offer -25% off this week</p>
+                <h2>Apple MacBook</h2>
+                <input type="button">
             </div>
         </div>
     </div>
