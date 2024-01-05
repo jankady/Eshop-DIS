dc16ae534f838acc957f6f6b7e40df92dd8ab00f
jankady <kaduch05@gmail.com>
2024-01-05 19:22:09 +0100

    creating product_detail.php dynamic for one product only

--- pages/product_detail.php
+++ pages/product_detail.php
@@ -1,1 +1,41 @@
 <?php
+require_once("../scripts/DBconnect.php");
+$conn = DBconnect::connectDatabase();
+$productId = $_GET['id'];
+$sql = "SELECT * FROM `product` WHERE `ID` = ? ";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param('i', $productId);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$product = $result->fetch_assoc();
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="../img/shop.ico">
+    <link rel="stylesheet" href="../style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Eshop-<?= $product["title"] ?></title>
+</head>
+<body>
+<?php
+require_once("../components/nav.php");
+?>
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"></script>
+</body>
+</html>
