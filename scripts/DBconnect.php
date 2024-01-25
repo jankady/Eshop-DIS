c5c391d006057a0fa99a3109f87b4c74dcea745a
jankady <kaduch05@gmail.com>
2024-01-25 17:54:14 +0100

    removing couting price script and adding calculation for product-card.php

--- scripts/DBconnect.php
+++ scripts/DBconnect.php
@@ -15,3 +15,4 @@ class DBconnect
         return $conn;
     }
 }
+?>
--- scripts/product-card.php
+++ scripts/product-card.php
@@ -4,9 +4,10 @@ class Product_card
 {
     public function product(): void
     {
-        require_once ("DBconnect.php");
+        require_once("DBconnect.php");
         $conn = DBconnect::connectDatabase();
-        $sql = "SELECT ID,`title`,`picture`,REPLACE(FORMAT(`price`, '000 000'), ',', ' ') as price,`number_of_products`,`ID_category`,`description` FROM product";
+        $sql = "SELECT product.*, sale.discount_percent as discount FROM `product` INNER JOIN sale ON product.ID_sale=sale.ID";
+//        $sql = "SELECT ID,`title`,`picture`,REPLACE(FORMAT(`price`, '000 000'), ',', ' ') as price,`number_of_products`,`ID_category`,`description`  FROM `product`";
         $result = mysqli_query($conn, $sql);
         ?>
         <div class="row">
@@ -15,11 +16,38 @@ class Product_card
                 ?>
                 <div class="col-4 mt-2 mb-2">
                     <div class="card" style="max-width: 25rem; max-height: 40rem">
-                        <a href="../pages/product_detail.php?id=<?= $row['ID'] ?>"> <h5 class="card-title"><?= $row['title'] ?></h5></a>
+                        <a href="../pages/product_detail.php?id=<?= $row['ID'] ?>"><h5
+                                    class="card-title"><?= $row['title'] ?></h5></a>
                         <div class="card-body">
-                            <a href="../pages/product_detail.php?id=<?= $row['ID'] ?>"><img src="../<?= $row['picture'] ?>" class="card-img-top" alt="<?= $row['title'] ?>" ></a>
+                            <a href="../pages/product_detail.php?id=<?= $row['ID'] ?>"><img
+                                        src="../<?= $row['picture'] ?>" class="card-img-top" alt="<?= $row['title'] ?>"></a>
                             <p class="card-text"><?= $row['description'] ?></p>
-                            <p class="card-subtitle"><?= $row['price'] ?> Kč</p>
+                            <div class="card-subtitle">
+<!--            Výpočet slevy a zobrazení slevy                    -->
+                                <div class="sale">
+                                    <?php
+                                    if ($row["ID_sale"] != 1) {
+                                        echo "SALE " . $row["discount"] . "%";
+                                    }
+                                    ?>
+                                </div>
+                                <div class="priceNumber">
+                                    <?php
+                                    $originalPrice = number_format($row["price"], 0, ',', ' ');
+
+                                    if ($row["ID_sale"] != 1) {
+                                        $finalNumber = $row["price"] * ($row["discount"] / 100);
+                                        $finalNumber = round($row["price"] - $finalNumber) . " Kč";
+                                        $finalNumberFloat = (float)$finalNumber;
+                                        $finalNumberFloat = number_format($finalNumberFloat, 0, ',', ' ');
+                                        ?>
+                                        <p><?= $finalNumberFloat ?> Kč</p>
+                                        <p class="fs-6 text-decoration-line-through"><?= $originalPrice ?> Kč</p>
+                                        <?php
+                                    } else echo $originalPrice . " Kč";
+                                    ?>
+                                </div>
+                            </div>
                             <a href="#" class="btn btn-primary">Add to cart</a>
                         </div>
                     </div>
