a00e8c7843e3cc2b643a57e866a0bb975f0fd180
jankady <kaduch05@gmail.com>
2024-03-17 17:30:32 +0100

    Jirka: adding login.php

---
+++ pages/login.php
@@ -0,0 +1,50 @@
+
+<!doctype html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="../img/shop.ico">
+    <link rel="stylesheet" href="../style/styleTesting.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Eshop</title>
+</head>
+<body>
+<?php
+require_once("../components/nav.php");
+require_once("../components/header.php");
+?>
+
+<!-- Formulář pro přihlášení -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <h2>Přihlášení</h2>
+            <?php if(isset($error_message)): ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $error_message; ?>
+                </div>
+            <?php endif; ?>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                <div class="mb-3">
+                    <label for="username" class="form-label">Uživatelské jméno nebo Email</label>
+                    <input type="text" class="form-control" id="username" name="username" required>
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Heslo</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Přihlásit</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php require_once ("../components/footer.php"); ?>
+
+<!-- Obsah pro patičku HTML -->
+</body>
+</html>
