4637f0ca509c168747b458bce49b4bd2f39d200c
Jiri Majak <majak.jiri06@gmail.com>
2024-04-12 10:35:51 +0200

    writing id and product id on screen in logined_cart.php

--- scripts/logined_cart.php
+++ scripts/logined_cart.php
@@ -1,4 +1,6 @@
 <?php
+require_once("Utility.php");
+
 if (isset($_POST['addToCart'])) {
 
     session_start();
@@ -10,35 +12,27 @@ if (isset($_POST['addToCart'])) {
     $user_id = $_SESSION["user_id"];
 
 
-    // Zkontrolovat, zda již košík existuje
-    if (isset($_SESSION["cart"])) {
-        // Vyhledat položku se stejným product_id
-        $found = false;
-        foreach ($_SESSION["cart"] as &$item) {
-            if ($item["product_id"] == $product_id) {
-                // Aktualizovat množství produktu
-                $item["quantity"] = isset($item["quantity"]) ? $item["quantity"] + 1 : 2; // Začít na 1 nebo 2
-                $found = true;
-                break;
-            }
-        }
+// Připravit SQL dotaz s funkcí MAX() pro získání největšího ID z tabulky shopping_cart
+    $sql = "SELECT MAX(ID) AS max_id FROM shopping_cart WHERE ID_customer = ?";
 
-        // Pokud produkt nebyl nalezen, přidat ho jako novou položku
-        if (!$found) {
-            $_SESSION["cart"][] = array(
-                'product_id' => $product_id,
-                'quantity' => 1
-            );
-        }
-    } else {
-        // Vytvořit košík, pokud ještě neexistuje
-        $_SESSION["cart"] = array(
-            array(
-                'product_id' => $product_id,
-                'quantity' => 1
-            )
-        );
-    }
+// Připravit příkaz pro provedení připraveného SQL dotazu
+    $stmt = mysqli_prepare($conn, $sql);
+        // Vázat parametr pro ID uživatele na dotaz
+        mysqli_stmt_bind_param($stmt, "i", $user_id);
+        // Provést dotaz
+        mysqli_stmt_execute($stmt);
+        // Získat výsledek dotazu
+        $result = mysqli_stmt_get_result($stmt);
+        // Získat řádek výsledku jako asociativní pole
+        $row = mysqli_fetch_assoc($result);
+        // Získat největší ID záznamu z tabulky shopping_cart
+        $max_id = $row['max_id'];
+        // Uzavřít příkaz
+        mysqli_stmt_close($stmt);
+
+        // Zde můžete provést další akce s $max_id, například ho využít pro další operace v aplikaci
+        echo "Největší ID záznamu v tabulce shopping_cart pro uživatele s ID $user_id je: $max_id";
+
 
 
     // stay on current site using javaScript
@@ -75,58 +69,58 @@ if (isset($_POST['removeFromCart'])) {
     exit();
 
 }
-session_start();
-
-// Získání ID produktu a nového množství z formuláře
-$product_id = $_POST['product_id'];
-$new_quantity = $_POST['quantity'];
-
-require_once("Utility.php");
-$conn = Utility::connectionDatabase();
-
-// Získání dostupného množství produktu z databáze
-$sql = "SELECT number_of_products FROM product WHERE ID = $product_id";
-$result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_assoc($result);
-$available_quantity = $row['number_of_products'];
-
-// Pokud je nové množství 0, odebereme položku z košíku
-if ($new_quantity == 0) {
-    foreach ($_SESSION["cart"] as $key => $item) {
-        if ($item['product_id'] == $product_id) {
-            unset($_SESSION["cart"][$key]);
-            break;
-        }
-    }
-
-    if (empty($_SESSION["cart"])) {
-        unset($_SESSION["cart"]);
-    }
-
-} else {
-    // Ověření, zda zadané množství nepřekračuje dostupné množství
-    if ($new_quantity <= $available_quantity) {
-        // Aktualizace množství produktu v session cart
-        foreach ($_SESSION["cart"] as &$item) {
-            if ($item['product_id'] == $product_id) {
-                $item['quantity'] = $new_quantity;
-                break;
-            }
-        }
-    } else {
-        // Pokud zadané množství překračuje dostupné množství, upravíme ho na maximálně dostupné množství
-        $new_quantity = $available_quantity;
-        // Aktualizace množství produktu v session cart
-        foreach ($_SESSION["cart"] as &$item) {
-            if ($item['product_id'] == $product_id) {
-                $item['quantity'] = $new_quantity;
-                break;
-            }
-        }
-    }
-}
-
-// Přesměrování na stejnou stránku (aktualizace)
-header("Location: ../pages/shoppingCart.php");
+//session_start();
+//
+//// Získání ID produktu a nového množství z formuláře
+//$product_id = $_POST['product_id'];
+//$new_quantity = $_POST['quantity'];
+//
+//require_once("Utility.php");
+//$conn = Utility::connectionDatabase();
+//
+//// Získání dostupného množství produktu z databáze
+//$sql = "SELECT number_of_products FROM product WHERE ID = $product_id";
+//$result = mysqli_query($conn, $sql);
+//$row = mysqli_fetch_assoc($result);
+//$available_quantity = $row['number_of_products'];
+//
+//// Pokud je nové množství 0, odebereme položku z košíku
+//if ($new_quantity == 0) {
+//    foreach ($_SESSION["cart"] as $key => $item) {
+//        if ($item['product_id'] == $product_id) {
+//            unset($_SESSION["cart"][$key]);
+//            break;
+//        }
+//    }
+//
+//    if (empty($_SESSION["cart"])) {
+//        unset($_SESSION["cart"]);
+//    }
+//
+//} else {
+//    // Ověření, zda zadané množství nepřekračuje dostupné množství
+//    if ($new_quantity <= $available_quantity) {
+//        // Aktualizace množství produktu v session cart
+//        foreach ($_SESSION["cart"] as &$item) {
+//            if ($item['product_id'] == $product_id) {
+//                $item['quantity'] = $new_quantity;
+//                break;
+//            }
+//        }
+//    } else {
+//        // Pokud zadané množství překračuje dostupné množství, upravíme ho na maximálně dostupné množství
+//        $new_quantity = $available_quantity;
+//        // Aktualizace množství produktu v session cart
+//        foreach ($_SESSION["cart"] as &$item) {
+//            if ($item['product_id'] == $product_id) {
+//                $item['quantity'] = $new_quantity;
+//                break;
+//            }
+//        }
+//    }
+//}
+//
+//// Přesměrování na stejnou stránku (aktualizace)
+//header("Location: ../pages/shoppingCart.php");
 
 
