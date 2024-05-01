3ce58ce68ce9007ffe9af2e5b8b40854a1546575
jankady <kaduch05@gmail.com>
2024-05-01 21:56:34 +0200

    real time checking availability

--- scripts/logined_cart.php
+++ scripts/logined_cart.php
@@ -103,32 +103,54 @@ if(isset($_POST['product_id'], $_POST['quantity'])) {
         $product_id = $_POST['product_id'];
         $quantity = $_POST['quantity'];
 
-        echo $product_id;
+        if($quantity == 0) {
+            // Pokud je množství 0, odstraňte produkt z nákupního košíku
+            $sql_delete_item = "DELETE FROM shopping_cart_item WHERE ID_cart = (SELECT MAX(ID) FROM shopping_cart WHERE ID_customer = ?) AND ID_product = ?";
+            $stmt_delete_item = mysqli_prepare($conn, $sql_delete_item);
+            mysqli_stmt_bind_param($stmt_delete_item, "ii", $user_id, $product_id);
+            mysqli_stmt_execute($stmt_delete_item);
+            mysqli_stmt_close($stmt_delete_item);
+        } else {
+            // Zkontrolujte počet dostupných produktů v databázi
+            $sql_check_availability = "SELECT number_of_products FROM product WHERE ID = ?";
+            $stmt_check_availability = mysqli_prepare($conn, $sql_check_availability);
+            mysqli_stmt_bind_param($stmt_check_availability, "i", $product_id);
+            mysqli_stmt_execute($stmt_check_availability);
+            $result_check_availability = mysqli_stmt_get_result($stmt_check_availability);
+            $row_check_availability = mysqli_fetch_assoc($result_check_availability);
+            $available_quantity = $row_check_availability['number_of_products'];
+            mysqli_stmt_close($stmt_check_availability);
 
-        echo $quantity;
+            // Pokud je požadované množství větší než počet dostupných produktů, upravte ho na maximální dostupné množství
+            if($quantity > $available_quantity) {
+                $quantity = $available_quantity;
+            }
 
-        // Získání nejvyššího ID nákupního košíku pro daného uživatele
-        $sql_cart = "SELECT MAX(ID) AS max_id FROM shopping_cart WHERE ID_customer = ?";
-        $stmt_cart = mysqli_prepare($conn, $sql_cart);
-        mysqli_stmt_bind_param($stmt_cart, "i", $user_id);
-        mysqli_stmt_execute($stmt_cart);
-        $result_cart = mysqli_stmt_get_result($stmt_cart);
-        $row_cart = mysqli_fetch_assoc($result_cart);
-        $cart_id = $row_cart['max_id'];
-        mysqli_stmt_close($stmt_cart);
+            // Získání nejvyššího ID nákupního košíku pro daného uživatele
+            $sql_cart = "SELECT MAX(ID) AS max_id FROM shopping_cart WHERE ID_customer = ?";
+            $stmt_cart = mysqli_prepare($conn, $sql_cart);
+            mysqli_stmt_bind_param($stmt_cart, "i", $user_id);
+            mysqli_stmt_execute($stmt_cart);
+            $result_cart = mysqli_stmt_get_result($stmt_cart);
+            $row_cart = mysqli_fetch_assoc($result_cart);
+            $cart_id = $row_cart['max_id'];
+            mysqli_stmt_close($stmt_cart);
 
-        // Aktualizace množství pouze u konkrétního produktu v nákupním košíku
-        $sql_update_quantity = "UPDATE shopping_cart_item SET quantity = ? WHERE ID_cart = ? AND ID_product = ?";
-        $stmt_update_quantity = mysqli_prepare($conn, $sql_update_quantity);
-        mysqli_stmt_bind_param($stmt_update_quantity, "iii", $quantity, $cart_id, $product_id);
-        mysqli_stmt_execute($stmt_update_quantity);
-        mysqli_stmt_close($stmt_update_quantity);
+            // Aktualizace množství pouze u konkrétního produktu v nákupním košíku
+            $sql_update_quantity = "UPDATE shopping_cart_item SET quantity = ? WHERE ID_cart = ? AND ID_product = ?";
+            $stmt_update_quantity = mysqli_prepare($conn, $sql_update_quantity);
+            mysqli_stmt_bind_param($stmt_update_quantity, "iii", $quantity, $cart_id, $product_id);
+            mysqli_stmt_execute($stmt_update_quantity);
+            mysqli_stmt_close($stmt_update_quantity);
+        }
 
         mysqli_close($conn);
         // Přesměrování zpět na stránku s nákupním košíkem
         header("Location: ../pages/shoppingCart.php");
         exit();
+    } else {
+        // Pokud uživatel není přihlášen, mělo by se nějakým způsobem ošetřit, co se stane, například přesměrováním na přihlašovací stránku
+        header("Location: ../pages/login.php");
+        exit();
     }
 }
-
-
