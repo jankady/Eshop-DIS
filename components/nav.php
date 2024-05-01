85f2f5e7f342a731edc57c7e14d05c9ab0258443
jankady <kaduch05@gmail.com>
2024-05-01 20:11:10 +0200

    Changes by Jiří Maják

--- components/nav.php
+++ components/nav.php
@@ -67,3 +67,125 @@
     </div>
 </nav>
 
+<?php
+/*
+ <?php
+// Předpokládejme, že máte databázové připojení a uživatel je přihlášen
+// Zde provedeme SQL dotaz, abychom zjistili, zda má uživatel něco v košíku
+$user_id = $_SESSION["user_id"]; // Předpokládáme, že máte uloženo ID přihlášeného uživatele
+
+$sql = "SELECT COUNT(*) as count FROM cart WHERE user_id = $user_id";
+// Spusťte dotaz na databázi, zkontrolujte, zda uživatel má něco v košíku
+// $result = mysqli_query($connection, $sql); // Předpokládáme, že používáte MySQLi
+
+// Zde předpokládáme, že máte uložené výsledky dotazu a proměnnou $cart_count, která obsahuje počet položek v košíku
+$cart_count = 5; // Například počet položek v košíku
+
+if ($_SESSION["logged_in"] == true) {
+    if ($cart_count > 0) {
+        ?>
+        <li><a href="../pages/shoppingCart.php"><img src="../img/shopCartFull.png" alt=""></a></li>
+        <?php
+    } else {
+        ?>
+        <li><a href="../pages/shoppingCart.php"><img src="../img/shopCartEmpty.png" alt=""></a></li>
+        <?php
+    }
+?>
+    <li>
+        <form method="post" action="../scripts/account.php">
+            <button type="submit" name="sign_out" class="btn btn-primary">Sign Out</button>
+        </form>
+    </li>
+<?php } else { ?>
+    <li><a href="../pages/login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    <li><a href="../pages/registration.php"><span class="glyphicon glyphicon-log-in"></span>
+            Register</a></li>
+<?php } ?>
+ */
+
+/*
+ *
+ * SELECT COUNT(sc_item.ID) AS cart_count
+FROM shopping_cart sc
+JOIN shopping_cart_item sc_item ON sc.ID = sc_item.ID_cart
+WHERE sc.ID_customer = :customer_id;
+iArma
+Amogus
+ArmasOS
+ *
+ * <?php
+// Předpokládejme, že databázový dotaz vrátil 3 položky v košíku
+$cart_count = 3;
+
+if ($_SESSION["logged_in"] == true) {
+  if ($cart_count > 0) {
+    ?>
+    <li><a href="../pages/shoppingCart.php"><img src="../img/shopCartFull.png" alt=""></a></li>
+    <?php
+  } else {
+    ?>
+    <li><a href="../pages/shoppingCart.php"><img src="../img/shopCartEmpty.png" alt=""></a></li>
+    <?php
+  }
+?>
+  <li>
+    <form method="post" action="../scripts/account.php">
+      <button type="submit" name="sign_out" class="btn btn-primary">Sign Out</button>
+    </form>
+  </li>
+<?php } else { ?>
+  <li><a href="../pages/login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+  <li><a href="../pages/registration.php"><span class="glyphicon glyphicon-log-in"></span>
+      Register</a></li>
+<?php } ?>
+
+SELECT COUNT(*) AS cart_count
+FROM cart
+WHERE user_id = :user_id;
+
+
+<?php
+// Předpokládejme, že máte databázové připojení a proměnnou $user_id s ID přihlášeného uživatele
+
+// Připravte dotaz
+$sql = "SELECT COUNT(*) AS cart_count FROM cart WHERE user_id = :user_id";
+$stmt = $pdo->prepare($sql);
+
+// Vložte parametry
+$stmt->bindParam(':user_id', $user_id);
+
+// Spusťte dotaz
+$stmt->execute();
+
+// Získejte výsledek
+$result = $stmt->fetch();
+
+// Získejte počet položek v košíku
+$cart_count = $result['cart_count'];
+
+// Podmíněné zobrazení obrázku košíku
+if ($_SESSION["logged_in"] == true) {
+  if ($cart_count > 0) {
+    ?>
+    <li><a href="../pages/shoppingCart.php"><img src="../img/shopCartFull.png" alt=""></a></li>
+    <?php
+  } else {
+    ?>
+    <li><a href="../pages/shoppingCart.php"><img src="../img/shopCartEmpty.png" alt=""></a></li>
+    <?php
+  }
+?>
+  <li>
+    <form method="post" action="../scripts/account.php">
+      <button type="submit" name="sign_out" class="btn btn-primary">Sign Out</button>
+    </form>
+  </li>
+<?php } else { ?>
+  <li><a href="../pages/login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+  <li><a href="../pages/registration.php"><span class="glyphicon glyphicon-log-in"></span>
+      Register</a></li>
+<?php } ?>
+ */
+?>
+
